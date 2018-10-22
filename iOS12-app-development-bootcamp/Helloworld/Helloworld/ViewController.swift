566e41ca1e9c6e2a6587206b43f783f6e6c5c3af
devyohan <ticonweb@gmail.com>
2018-10-22 19:16:31 +0900

    Init first project

---
+++ iOS12-app-development-bootcamp/Helloworld/Helloworld/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Helloworld
+//
+//  Created by 박요한 on 22/10/2018.
+//  Copyright © 2018 Yohan Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+
+}
+
