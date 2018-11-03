40922e80c3dcd6933fe2e89e79e6c4e92578a0ec
devyohan <ticonweb@gmail.com>
2018-11-03 23:43:23 +0900

    Init a 2nd project

---
+++ iOS12-app-development-bootcamp/Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by 박요한 on 03/11/2018.
+//  Copyright © 2018 Yohan Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
