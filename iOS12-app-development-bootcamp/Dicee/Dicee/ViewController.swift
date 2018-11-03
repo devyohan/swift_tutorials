95f3c445db96df5d3195f109624a6029d506830d
devyohan <ticonweb@gmail.com>
2018-11-03 23:57:32 +0900

    Modified project settings and logic code

--- iOS12-app-development-bootcamp/Dicee/Dicee/ViewController.swift
+++ iOS12-app-development-bootcamp/Dicee/Dicee/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let diceImages = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    var dice1: Int = 0
+    var dice2: Int = 0
 
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rollButton(_ sender: UIButton) {
+    }
+    
+    func rollDice() {
+        dice1 = Int.random(in: 0 ... 5)
+        dice2 = Int.random(in: 0 ... 5)
+        
+    }
+    
 }
 
