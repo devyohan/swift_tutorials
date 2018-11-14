fbb53bfb4bb51c89767a90a678fa8582d6cfdd71
devyohan <ticonweb@gmail.com>
2018-11-14 20:59:15 +0900

    Init camera access

--- iOS12-app-development-bootcamp/SeeFood/SeeFood/ViewController.swift
+++ iOS12-app-development-bootcamp/SeeFood/SeeFood/ViewController.swift
@@ -11,15 +11,21 @@ import CoreML
 import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
-        
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBOutlet weak var cameraView: UIImageView!
     
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
 
 
