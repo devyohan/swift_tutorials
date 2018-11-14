a7e37f36463c17058167a2a008af42da2a293a8f
devyohan <ticonweb@gmail.com>
2018-11-14 20:41:15 +0900

    CoreML project

---
+++ iOS12-app-development-bootcamp/SeeFood/SeeFood/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by 박요한 on 14/11/2018.
+//  Copyright © 2018 Yohan Park. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
